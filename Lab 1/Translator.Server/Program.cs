61df6410388131134ca51d73b97a2208ebf7bb5d
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-01 20:14:00 +0000

    Initial commit for Lab 1

---
+++ Lab 1/Translator.Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace Translator.Server
+{
+    internal static class Program
+    {
+        private static int Main(String[] args)
+        {
+            return 0;
+        }
+    }
+}
