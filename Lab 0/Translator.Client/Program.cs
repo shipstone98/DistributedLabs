37ca0fd4c945f65776ed182cde3ddb92d44977e1
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-01 19:15:59 +0000

    Added client sample code

---
+++ Lab 0/Translator.Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Translator.Client
+{
+    internal static class Program
+    {
+        private const int EncryptionKey = 1;
+        private const String Hostname = "localhost";
+        private const ushort Port = 5002;
+
+        private static int Main(String[] args)
+        {
+            Console.Write("Please enter the message: ");
+            String message = Console.ReadLine();
+            byte[] rawData = Program.Serialize(message);
+            
+            using (TcpClient client = new TcpClient())
+            {
+                client.Connect(Program.Hostname, Program.Port);
+                NetworkStream ns = client.GetStream();
+                ns.Write(rawData, 0, rawData.Length);
+                ns.Flush();
+                ns.Close();
+            }
+
+            Console.Write("Press any key to quit...");
+            Console.ReadKey(true);
+            Console.WriteLine();
+            return 0;
+        }
+
+        private static byte[] Serialize(String message)
+        {
+            byte[] ascii = Encoding.ASCII.GetBytes(message), rawData = new byte[ascii.Length + 9];
+            int index = 0;
+            rawData[index ++] = 0;
+            rawData[index ++] = 0;
+            rawData[index ++] = 0;
+            rawData[index ++] = 1;
+            int remainingLength = ascii.Length + 3;
+            rawData[index ++] = (byte) ((remainingLength & 0xFF00) >> 8);
+            rawData[index ++] = (byte) (remainingLength & 0xFF);
+            rawData[index ++] = (byte) Program.EncryptionKey;
+            rawData[index ++] = (byte) ((ascii.Length & 0xFF00) >> 8);
+            rawData[index ++] = (byte) (ascii.Length & 0xFF);
+            Array.Copy(ascii, 0, rawData, index, ascii.Length);
+            return rawData;
+        }
+    }
+}
