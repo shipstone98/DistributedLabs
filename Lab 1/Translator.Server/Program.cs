ceba1bbdb5c9e35004b06a4661ab227146ec38c6
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-01 20:16:34 +0000

    Registered service

--- Lab 1/Translator.Server/Program.cs
+++ Lab 1/Translator.Server/Program.cs
@@ -7,8 +7,17 @@ namespace Translator.Server
 {
     internal static class Program
     {
+        private const ushort Port = 5002;
+
         private static int Main(String[] args)
         {
+            TcpChannel channel = new TcpChannel(Program.Port);
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof (Translator), "Translate", WellKnownObjectMode.SingleCall);
+            Console.WriteLine("Channel service is now registered");
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
+            Console.WriteLine();
             return 0;
         }
     }
