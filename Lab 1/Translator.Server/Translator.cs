e2638b272c4d4a999d9d2e41f3e49953f891c750
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-01 20:14:05 +0000

    Added Translate methods

---
+++ Lab 1/Translator.Server/Translator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Translator.Server
+{
+    internal class Translator: MarshalByRefObject
+    {
+        internal static String Translate(String englishString) => String.IsNullOrWhiteSpace(englishString) ? "" : Translator.Translate(englishString.Split(' '));
+
+        internal static String Translate(String[] englishString)
+        {
+            if (englishString is null)
+            {
+                throw new ArgumentNullException(nameof (englishString));
+            }
+
+            String result = "";
+
+            foreach (String word in englishString)
+            {
+                if (String.IsNullOrWhiteSpace(word))
+                {
+                    continue;
+                }
+
+                result += word.Substring(1);
+                result += word.Substring(0, 1) + "ay ";
+            }
+
+            return result.Trim();
+        }
+    }
+}
