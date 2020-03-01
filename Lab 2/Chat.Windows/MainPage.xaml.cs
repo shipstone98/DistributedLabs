a10b5778322b76f9ff35aa9d1c57b07616cbaf87
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-01 23:15:02 +0000

    Added event handlers

--- Lab 2/Chat.Windows/MainPage.xaml.cs
+++ Lab 2/Chat.Windows/MainPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +17,32 @@ namespace Chat.Windows
         public MainPage()
         {
             this.InitializeComponent();
+            this.UsernameTextBox.Focus(FocusState.Keyboard);
+        }
+
+        private void MessageTextBox_KeyDown(Object sender, KeyRoutedEventArgs e)
+        {
+            this.SendButton_ClickAsync(this.SendButton, new RoutedEventArgs());
+        }
+
+        private async void SendButton_ClickAsync(Object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(this.UsernameTextBox.Text))
+            {
+                MessageDialog md = new MessageDialog("You must enter your username.", "Error!");
+                await md.ShowAsync();
+            }
+
+            if (String.IsNullOrWhiteSpace(this.MessageTextBox.Text))
+            {
+                MessageDialog md = new MessageDialog("You must enter a message.", "Error!");
+                await md.ShowAsync();
+            }
+        }
+
+        private void UsernameTextBox_KeyDown(Object sender, KeyRoutedEventArgs e)
+        {
+            this.MessageTextBox.Focus(FocusState.Keyboard);
         }
     }
 }
