73a2efe9c973a6730105a976595da7a0943e077e
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-02 13:50:59 +0000

    Added getInt and getString

--- Lab 5/Translation.WebApi/Controllers/TranslateController.cs
+++ Lab 5/Translation.WebApi/Controllers/TranslateController.cs
@@ -13,8 +13,11 @@ namespace Translation.WebApi.Controllers
     {
         [HttpGet]
         public String Get() => "Hello World";
-        
-        [HttpGet("{id}", Name = "get")]
+
+        [HttpGet("{id:int}", Name = "getInt")]
         public String Get(int id) => "Your number plus 100 is " + id;
+
+        [HttpGet("{input}", Name = "getString")]
+        public String Get(String input) => "You sent the string " + input;
     }
 }
