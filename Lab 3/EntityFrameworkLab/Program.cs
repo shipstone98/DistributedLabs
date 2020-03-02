ffdc7c37d543db784870851b98ca6d20740ebbd5
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-02 00:41:51 +0000

    Initial commit for Lab 3

---
+++ Lab 3/EntityFrameworkLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
