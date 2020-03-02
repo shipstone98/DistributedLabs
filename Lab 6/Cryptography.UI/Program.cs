d7c98d44ce6b0ec690f9bd875d749fecc3b9a0b1
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-02 18:42:44 +0000

    Added RSA sample code

--- Lab 6/Cryptography.UI/Program.cs
+++ Lab 6/Cryptography.UI/Program.cs
@@ -27,17 +27,63 @@ namespace Cryptography.UI
             byte[] asciiByteMessage = Encoding.ASCII.GetBytes(MESSAGE);
             SHA1 sha1Provider = new SHA1CryptoServiceProvider();
             byte[] sha1Message = sha1Provider.ComputeHash(asciiByteMessage);
-            Console.WriteLine($"Decrypted message: {MESSAGE}");
-            Console.WriteLine($"Encrypted message: {Program.ByteArrayToHexString(sha1Message)}");
+            Console.WriteLine($"Original message: {MESSAGE}");
+            Console.WriteLine($"Encrypted message using SHA1: {Program.ByteArrayToHexString(sha1Message)}");
+            byte[] encryptedByteMessage, decryptedByteMessage;
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                encryptedByteMessage = Program.RSAEncrypt(asciiByteMessage, rsa.ExportParameters(false));
+                decryptedByteMessage = Program.RSADecrypt(encryptedByteMessage, rsa.ExportParameters(true));
+            }
             
+            Console.WriteLine($"Encrypted message using RSA: {Program.ByteArrayToHexString(encryptedByteMessage)}");
+            Console.WriteLine($"Decrypted message using RSA: {Encoding.ASCII.GetString(decryptedByteMessage)}");
+
             if (!Console.IsInputRedirected)
             {
                 Console.Write("Press any key to quit...");
                 Console.ReadKey(true);
                 Console.WriteLine();
             }
-            
+
             return 0;
+        }
+
+        private static byte[] RSADecrypt(byte[] dataToDecrypt, RSAParameters rsaKeyInfo)
+        {
+            try
+            {
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.ImportParameters(rsaKeyInfo);
+                    return rsa.Decrypt(dataToDecrypt, false);
+                }
+            }
+
+            catch (CryptographicException)
+            {
+                Console.WriteLine("ERROR: couldn't decrypt data");
+                return null;
+            }
+        }
+
+        private static byte[] RSAEncrypt(byte[] dataToDecrypt, RSAParameters rsaKeyInfo)
+        {
+            try
+            {
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.ImportParameters(rsaKeyInfo);
+                    return rsa.Encrypt(dataToDecrypt, false);
+                }
+            }
+
+            catch (CryptographicException)
+            {
+                Console.WriteLine("ERROR: couldn't encrypt data");
+                return null;
+            }
         }
     }
 }
