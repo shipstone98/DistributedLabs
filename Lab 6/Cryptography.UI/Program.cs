5f3228e3b4e621bba421eb331e9182a0e461c58c
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-02 17:59:13 +0000

    Added SHA1 encryption

--- Lab 6/Cryptography.UI/Program.cs
+++ Lab 6/Cryptography.UI/Program.cs
@@ -1,11 +1,42 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Cryptography.UI
 {
     internal static class Program
     {
+        private static String ByteArrayToHexString(byte[] byteArray)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (!(byteArray is null && byteArray.Length == 0))
+            {
+                foreach (byte b in byteArray)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+            }
+
+            return sb.ToString();
+        }
+
         private static int Main(String[] args)
         {
+            const String MESSAGE = "Hello World!";
+            byte[] asciiByteMessage = Encoding.ASCII.GetBytes(MESSAGE);
+            SHA1 sha1Provider = new SHA1CryptoServiceProvider();
+            byte[] sha1Message = sha1Provider.ComputeHash(asciiByteMessage);
+            Console.WriteLine($"Decrypted message: {MESSAGE}");
+            Console.WriteLine($"Encrypted message: {Program.ByteArrayToHexString(sha1Message)}");
+            
+            if (!Console.IsInputRedirected)
+            {
+                Console.Write("Press any key to quit...");
+                Console.ReadKey(true);
+                Console.WriteLine();
+            }
+            
             return 0;
         }
     }
