9673c545c412db2ff21efcc5395e580ee809c8da
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-02 13:55:26 +0000

    Added ActionName

--- Lab 5/Translation.WebApi/Controllers/TranslateController.cs
+++ Lab 5/Translation.WebApi/Controllers/TranslateController.cs
@@ -8,16 +8,20 @@ using System.Threading.Tasks;
 namespace Translation.WebApi.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class TranslateController : ControllerBase
     {
         [HttpGet]
         public String Get() => "Hello World";
 
+        [ActionName("GetInt")]
         [HttpGet("{id:int}", Name = "getInt")]
         public String Get(int id) => "Your number plus 100 is " + id;
 
+        [HttpGet("{name}", Name = "getName")]
+        public String GetName(String name) => "Your name is " + name;
+
         [HttpGet("{input}", Name = "getString")]
-        public String Get(String input) => "You sent the string " + input;
+        public String GetString(String input) => "You sent the string " + input;
     }
 }
