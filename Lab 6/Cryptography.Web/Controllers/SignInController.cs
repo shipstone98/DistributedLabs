7ee6f77854219c3d8a0a3f9aa97b3af66b3a67af
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-10 12:08:32 +0000

    Added sign in page

---
+++ Lab 6/Cryptography.Web/Controllers/SignInController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cryptography.Web.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult Index() => this.View();
+
+        public IActionResult Login() => this.Challenge(new AuthenticationProperties()
+        {
+            RedirectUri = "/signin/index"
+        }, "Microsoft");
+    }
+}
